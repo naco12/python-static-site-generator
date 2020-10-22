2bc293a4bc3f884a9e74106bee90d45148d02df6
naco12 <nacop09@gmail.com>
2020-10-22 16:14:06 -0500

    completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -15,6 +15,6 @@ class Parser:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
-        full_path = self.dest / path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
