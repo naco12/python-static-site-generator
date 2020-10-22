b6724273b7aea9939b14741c182f5ce23e7d5e9e
naco12 <nacop09@gmail.com>
2020-10-22 17:35:09 -0500

    completed module

--- ssg/site.py
+++ ssg/site.py
@@ -18,8 +18,8 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-                
-    def load_parser(extension):
+
+    def load_parser(self, extension):
         for parser in self.parsers:
             if extension.valid_extension():
                 return parser
