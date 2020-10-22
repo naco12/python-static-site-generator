2e443833ee02010c5ec5646f8982737fc23aeb4f
naco12 <nacop09@gmail.com>
2020-10-22 07:57:06 -0500

    completed module

--- ssg/site.py
+++ ssg/site.py
@@ -5,10 +5,15 @@ class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
-
+        
+    # make a directory
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
+    # make the destination directory
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                path.create_dir()
