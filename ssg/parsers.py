1027b17647a52930b85334842b3a2c4f063d81c3
naco12 <nacop09@gmail.com>
2020-10-22 15:32:10 -0500

    completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -2,4 +2,10 @@ from typing import List
 from pathlib import Path
 
 class Parser:
-    extensions: List[str] = [] 
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError()
