eb9b0319a8fe8305d65b5dd6d7505649fb275c65
naco12 <nacop09@gmail.com>
2020-10-22 15:49:30 -0500

    completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,3 +9,7 @@ class Parser:
 
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
+
+    def read(path):
+        with open(self, path, "r") as file:
+            return file.read()
