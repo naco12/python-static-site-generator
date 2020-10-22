72accd742a90f63d3321c257931eccad4a3cae70
naco12 <nacop09@gmail.com>
2020-10-22 07:22:35 -0500

    completed module

--- ssg/site.py
+++ ssg/site.py
@@ -5,3 +5,6 @@ class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / relative_to(self.source)
