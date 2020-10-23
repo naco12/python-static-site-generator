8636325b8506bc3003ddf6882524ddbd7ca40785
naco12 <nacop09@gmail.com>
2020-10-23 08:59:19 -0500

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -7,7 +7,24 @@ class Content(Mapping):
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
-    def load(self, cls, string):
+    def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
+
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
