4e7bf4c1f38a74f7be3c90dea0cae37e31b0ffb4
naco12 <nacop09@gmail.com>
2020-10-23 10:41:19 -0500

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -40,3 +40,8 @@ class MarkdownParser(Parser):
             html = markdown(content.body)
             self.write(path, dest, html)
             sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+class ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
